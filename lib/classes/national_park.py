ff3ef93605cd742d3cf43a53538fcd5d27489133
Tyler Taylor <vikingtyty@gmail.com>
2023-06-20 14:39:28 -0500

    Complete

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,17 +4,55 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, 'name'):
+            self._name = name
+        else:
+            raise Exception
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        
+        if isinstance(new_visitor, Visitor) and new_visitor not in self._visitors:
+            self._visitors.append(new_visitor)
+        return self._visitors
     
     def total_visits(self):
-        pass
+        # Returns the total number of times that park has been visited
+        return len(self._trips)
     
     def best_visitor(self):
+        # Returns the Visitor who has visited the park the most
+
+        # Create a baseline / starting point to compare with:
+        max_visitor = None
+        max_visits = 0
+
+        for visitor in self._visitors:
+            # for each trip in our trips list
+            #   check if the associated visitor is the visitor from our current loop iteration
+
+            visits = len([trip for trip in self._trips if trip.visitor == visitor])
+            # below is the same general logic, just flipped around a bit
+            # visits = len([trip for trip in visitor._trips if trip._national_park == self])
+
+            if visits > max_visits:
+                max_visitor = visitor
+                max_visits = visits
+        
+        return max_visitor
+
         pass
--- lib/classes/trip.py
+++ lib/classes/trip.py
@@ -6,3 +6,38 @@ class Trip:
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+
+        # We need to create our relationships here
+        self.visitor.trips(self)
+        self.visitor.national_parks(self.national_park)
+        # visitor.national_parks(national_park)
+        
+        national_park.trips(self)
+        national_park.visitors(visitor)
+
+    def get_visitor(self):
+        return self._visitor
+
+    def set_visitor(self, visitor):
+        # Must be of type Visitor
+        from classes.visitor import Visitor
+
+        if visitor and isinstance(visitor, Visitor):
+            self._visitor = visitor
+        else:
+            raise Exception
+
+    visitor = property(get_visitor, set_visitor)
+
+    @property
+    def national_park(self):
+        return self._national_park
+
+    @national_park.setter
+    def national_park(self, national_park):
+        from classes.national_park import NationalPark
+
+        if national_park and isinstance(national_park, NationalPark):
+            self._national_park = national_park
+        else:
+            raise Exception
